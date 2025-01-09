67d9110e4ecc3602623eb2199f68df64c75f4021
Andres <esteban.altamura@gmail.com>
2025-01-09 17:13:34 +0100

    update

--- src/app/_noPages/views/home/profile/Profile.tsx
+++ src/app/_noPages/views/home/profile/Profile.tsx
@@ -280,6 +280,10 @@ const Profile = () => {
               </Box>
             </CustomTooltip>
 
+
+            {/* */}
+
+
             <a href='https://drive.google.com/file/d/1zWEyXIiC_N5Olp-FrZAPF23WslwYMd1Q/view?usp=sharing'>
               <CustomTooltip
                 title='Descargar cv'
