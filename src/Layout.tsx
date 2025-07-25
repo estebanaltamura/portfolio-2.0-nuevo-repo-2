2cc22a680dd7a22781862b800fb0383c8c0b028e
Andres Altamura <esteban.altamura@gmail.com>
2025-07-25 20:16:08 +0200

    update

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -16,8 +16,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <body className="font-openSans font-russoFont font-interFont">
         <main>{children}</main>
         <footer style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '10px', padding: '60px 0', width: '100%', position: 'relative' }}>
-          <div style={{ position: 'absolute', top: '0', left: '50%', transform: 'translateX(-50%)', width: 'calc(100vw - 15px)', backgroundColor: 'white', height: '2px' }}></div>
-          <p style={{ color: 'white', fontSize: '16px' }}>esteban.altamura@gmail.com</p>
+          <div style={{ position: 'absolute', top: '0', width: 'calc(100vw - 15px)', backgroundColor: 'white', height: '2px' }}></div>
+          <p style={{ color: 'white', fontSize: '15px' }}>esteban.altamura@gmail.com</p>
           <a href="/claims-and-refunds-policy" style={{ fontSize: '14px', color: 'white', paddingBottom: '2px', borderBottom: '1px solid white' }}>
             Políticas de Disputas y Reembolsos
           </a>
