e9f9da0a6083cfcc1eecf06fe930e56ec266f9d7
Andres Altamura <esteban.altamura@gmail.com>
2025-07-25 20:05:56 +0200

    overflow

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <body className="font-openSans font-russoFont font-interFont">
         <main>{children}</main>
         <footer style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '10px', padding: '80px 0', marginTop: '40px', width: '100%', position: 'relative' }}>
-          <div style={{ position: 'absolute', top: '0', left: '50%', transform: 'translateX(-50%)', width: '100vw', backgroundColor: 'white', height: '2px' }}></div>
+          <div style={{ position: 'absolute', top: '0', left: '50%', transform: 'translateX(-50%)', width: 'calc(100vw - 15px)', backgroundColor: 'white', height: '2px' }}></div>
           <p style={{ color: 'white' }}>esteban.altamura@gmail.com</p>
           <a href="/claims-and-refunds-policy" style={{ fontSize: '14px', color: 'white', paddingBottom: '2px', borderBottom: '1px solid white' }}>
             Políticas de Disputas y Reembolsos
